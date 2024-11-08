a706dc118538d701d5206c16858c1340d37050b7
taha <amirtahafarshbaf@gmail.com>
2024-11-08 16:06:17 +0330

    variebels name changed

--- RIP.py
+++ RIP.py
@@ -1,144 +1,152 @@
+# import libraries
 import numpy as np
+from scipy import optimize
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
 import matplotlib.animation as animation
 from matplotlib.patches import Rectangle
 from math import pi, sin, cos, pow
 
-# SYSTEM PARAMETERS
-g = 9.8   # GRAVITY
-L_p = 1.0   # PENDULUM LENGTH (m)
-L_a = 1.0   # ARM LENGTH (m)
-m1 = 0.5   # PENDULUM MASS (kg)
+# system parameters
+g   = 9.8   # gravity
+L_p = 1.0   # pendulum length (m)
+L_a = 1.0   # arm length (m)
+m1  = 0.5   # pendulum mass (kg)
 I_a = 0     # Moment of inertia of the arm
-I_p = 0     # Moment of inertia of the PENDULUM
-mc = 0           # Location of the center of mass of the pendulum
+I_p = 0     # Moment of inertia of the pendulum
+mc  = 0     # Location of the center of mass of the pendulum
 
-# SIMULATION PARAMETERS
+# simulation parameters
 dt = 0.05
 Tmax = 35
 t = np.arange(0.0, Tmax, dt)
 
-# INITIAL CONDITIONS
-dtheta = .0      # PENDULUM ANGULAR VELOCITY
-theta = pi - 0.1   # PWNDULUM INITIAL ANGEL
-alpha = .0      # ARM INITIAL ANGEL
-x0 = 0          # موقعیت هدف کالسکه
-dalpha= -0.05   # ARM INITIAL SPEED
-k = 0.08        # ENERGY CONTROL GAIN
+# initail condition
+theta = pi - 0.1  # pendulum initial angel
+dtheta = .0       # pendulum initial speed
+alpha = .0        # arm initial angle
+x0 = 0            # موقعیت هدف کالسکه
+dalpha= -0.05     # arm initial speed
+k = 0.08          # energy control gain
 
-# CONTROLLER GAINS
+# PID controller gains
 Kp_theta = 50
 Kd_theta = 15
 Kp_alpha = 3.1
 Kd_alpha = 4.8
 
-# وضعیت اولیه
+# creating initial state
 state = np.array([theta, dtheta, alpha, dalpha])
 stabilizing = False
 
-# متغیر برای ذخیره نیروی کنترلی
+# saving force control values
 u_values = []
 
-def energy(th, dth):
+# energy calculation
+def energy(th, dth): 
     return 0.5 * (I_a * pow(alpha,2) + m1 * pow(m1,2) + I_p * pow(dtheta,2)) + (m1 * g * L_p * (cos(theta) - 1))
 
+# siwtch for control or swing up mode
 def isControllable(th, dth):
     return th < pi/9 and abs(energy(th, dth)) < 0.5
 
+
 def derivatives(state, t):
     global stabilizing
-    ds = np.zeros_like(state)
-    _theta = state[0]
-    _dtheta = state[1]  # سرعت زاویه‌ای
-    _alpha = state[2]
-    _dalpha = state[3]  # سرعت کالسکه
+    ds = np.zeros_like(state) #?
+    _theta  = state[0]  # pendulum angle
+    _dtheta = state[1]  # pendulum velocity
+    _alpha  = state[2]  # arm angle
+    _dalpha = state[3]  # arm velovity
 
-    # کنترل با توجه به وضعیت پایدارسازی
+    # control switch based on energy
     if stabilizing or isControllable(_theta, _dtheta):
         stabilizing = True
-        u = Kp_theta * _theta + Kd_theta * _dtheta + Kp_alpha * (_alpha - x0) + Kd_alpha * _dalpha
+        u = Kp_theta * _theta + Kd_theta * _dtheta + Kp_alpha * (_alpha - x0) + Kd_alpha * _dalpha #?
     else:
         E = energy(_theta, _dtheta)
         u = k * E * _dtheta * cos(_theta)
 
-    u_values.append(u)  # ذخیره نیروی کنترلی
+    u_values.append(u)  
 
     ds[0] = state[1]
     ds[1] = (g * sin(_theta) - u * cos(_theta)) / L_p
     ds[2] = state[3]
     ds[3] = u
 
-    return ds
+    return ds #?
 
-# شبیه‌سازی
-# شبیه‌سازی
+# simulation
 print("Integrating...")
 solution = integrate.odeint(derivatives, state, t)
 print("Done")
 
-# بررسی و تطبیق اندازه u_values و solution
+# array size check for solution and u
 if len(u_values) > len(t):
     u_values = u_values[:len(t)]
 elif len(u_values) < len(t):
     t = t[:len(u_values)]
 
-# استخراج مقادیر حالت‌ها از solution
-ths = solution[:, 0]
-Ys = solution[:, 1]
-xs = solution[:, 2]
-Zs = solution[:, 3]
+# state variebles from ODE solution
+theta_s  = solution[:, 0]
+dtheta_s = solution[:, 1]
+alpha_s  = solution[:, 2]
+dalpha_s = solution[:, 3]
 
-# رسم نمودارهای پارامترهای کنترلی
-plt.figure(figsize=(12, 10))
 
-# نمودار زاویه پاندول
+plt.figure(figsize=(12, 12))
+
+# pendulum angle plot
 plt.subplot(3, 2, 1)
-plt.plot(t, ths, label="Angle (θ)")
+plt.plot(t, theta_s, label="Angle (θ)")
 plt.xlabel("Time (s)")
 plt.ylabel("θ (rad)")
 plt.title("Pendulum Angle (θ) over Time")
+plt.grid()
 plt.legend()
 
-# نمودار سرعت زاویه‌ای
+# pendulum velocity plot
 plt.subplot(3, 2, 2)
-plt.plot(t, Ys, label="Angular Velocity (θ')", color='orange')
+plt.plot(t, dtheta_s, label="Angular Velocity (θ')", color='orange')
 plt.xlabel("Time (s)")
 plt.ylabel("θ' (rad/s)")
 plt.title("Angular Velocity (θ') over Time")
+plt.grid()
 plt.legend()
 
-# نمودار موقعیت کالسکه
+# arm position plot
 plt.subplot(3, 2, 3)
-plt.plot(t, xs, label="Cart Position (x)", color='green')
+plt.plot(t, alpha_s, label="Cart Position (x)", color='green')
 plt.xlabel("Time (s)")
 plt.ylabel("x (m)")
 plt.title("Cart Position (x) over Time")
+plt.grid()
 plt.legend()
 
-# نمودار سرعت کالسکه
+# arm velocity 
 plt.subplot(3, 2, 4)
-plt.plot(t, Zs, label="Cart Velocity (x')", color='red')
+plt.plot(t, dalpha_s, label="Cart Velocity (x')", color='red')
 plt.xlabel("Time (s)")
 plt.ylabel("x' (m/s)")
 plt.title("Cart Velocity (x') over Time")
 plt.legend()
+plt.grid()
 
-# نمودار نیروی کنترلی
+# control force plot
 plt.subplot(3, 2, 5)
 plt.plot(t, u_values, label="Control Force (u)", color='purple')
 plt.xlabel("Time (s)")
 plt.ylabel("u (N)")
 plt.title("Control Force (u) over Time")
 plt.legend()
-
+plt.grid()
 plt.tight_layout()
 plt.show()
 
 
 # پارامترهای شبیه‌سازی گرافیکی پاندول
-pxs = L_p * np.sin(ths) + xs
-pys = L_p * np.cos(ths)
+pxs = L_p * np.sin(theta_s) + alpha_s
+pys = L_p * np.cos(theta_s)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, autoscale_on=False, xlim=(-1.5, 1.5), ylim=(-1.2, 1.2))
@@ -166,13 +174,13 @@ def init():
     return line, time_text, energy_text, patch
 
 def animate(i):
-    thisx = [xs[i], pxs[i]]
+    thisx = [alpha_s[i], pxs[i]]
     thisy = [0, pys[i]]
     line.set_data(thisx, thisy)
     time_text.set_text(time_template % (i * dt))
-    E = energy(ths[i], Ys[i])
+    E = energy(theta_s[i], dtheta_s[i])
     energy_text.set_text(energy_template % (E))
-    patch.set_x(xs[i] - cart_width / 2)
+    patch.set_x(alpha_s[i] - cart_width / 2)
     return line, time_text, energy_text, patch
 
 ani = animation.FuncAnimation(fig, animate, np.arange(1, len(solution)), interval=25, blit=True, init_func=init)
